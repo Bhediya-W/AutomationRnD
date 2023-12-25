415c797405d9fdda1c6d998fbca00ebf6acecae8
Amit Khirodwala <107615974+amitkhirodwala@users.noreply.github.com>
2023-12-25 19:53:44 +0530

    google-com-test-case-1

--- e2e/Projects/google/first-test-case.spec.ts
+++ e2e/Projects/google/first-test-case.spec.ts
@@ -3,6 +3,6 @@ import {test, expect } from "@playwright/test";
 test('has title', async ({page}) => {
   await page.goto('https://google.com/');
   await expect(page).toHaveTitle(/Google/);
-
-
+  await page.getByLabel('Search', {exact: true }).fill('Amit Khirodwala');
+  await page.getByRole('button',{name: 'Google Search'}).click();
 })
